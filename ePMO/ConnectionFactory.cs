0f6b57b05648e6a878c65275efaf87caa3fdc434
zubair <zubair@utmspace.edu.my>
2017-11-03 15:28:53 +0800

    Add connection factory class

---
+++ ePMO/ConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ePMO
+{
+    public static class ConnectionFactory
+    {
+        public static SqlConnection GetConnection()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ePMO"].ToString();
+            return new SqlConnection(connectionString);
+        }
+    }
+}
